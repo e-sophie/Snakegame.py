4c5b3423ac5545cd30fd73f522f52db864f48519
e-sophie <essiensophie6@gmail.com>
2025-08-28 21:47:20 +0100

    Initial commit

--- snakegame.py
+++ snakegame.py
@@ -7,18 +7,98 @@ from pygame.math import Vector2
 class SNAKE:
     def __init__(self):
         self.body = [Vector2(5, 10), Vector2(6, 10), Vector2(7, 10)]
-        self.direction = Vector2(1, 0)
+        self.direction = Vector2(-1, 0)
         self.new_block = False
 
+        # Load head images
+        self.head_up = pygame.image.load(
+            'graphics/head_up.png').convert_alpha()
+        self.head_down = pygame.image.load(
+            'graphics/head_down.png').convert_alpha()
+        self.head_right = pygame.image.load(
+            'graphics/head_right.png').convert_alpha()
+        self.head_left = pygame.image.load(
+            'graphics/head_left.png').convert_alpha()
+
+        # Load tail images
+        self.tail_up = pygame.image.load(
+            'graphics/tail_up.png').convert_alpha()
+        self.tail_down = pygame.image.load(
+            'graphics/tail_down.png').convert_alpha()
+        self.tail_right = pygame.image.load(
+            'graphics/tail_right.png').convert_alpha()
+        self.tail_left = pygame.image.load(
+            'graphics/tail_left.png').convert_alpha()
+
+        # Load body images
+        self.body_vertical = pygame.image.load(
+            'graphics/body_vertical.png').convert_alpha()
+        self.body_horizontal = pygame.image.load(
+            'graphics/body_horizontal.png').convert_alpha()
+        self.body_tr = pygame.image.load(
+            'graphics/body_tr.png').convert_alpha()
+        self.body_tl = pygame.image.load(
+            'graphics/body_tl.png').convert_alpha()
+        self.body_br = pygame.image.load(
+            'graphics/body_br.png').convert_alpha()
+        self.body_bl = pygame.image.load(
+            'graphics/body_bl.png').convert_alpha()
+
     def draw_snake(self):
-        for block in self.body:
+        self.update_head_graphics()
+        self.update_tail_graphics()
+
+        for index, block in enumerate(self.body):
             x_pos = int(block.x * cell_size)
             y_pos = int(block.y * cell_size)
             block_rect = pygame.Rect(x_pos, y_pos, cell_size, cell_size)
-            pygame.draw.rect(screen, (183, 111, 122), block_rect)
+
+            if index == 0:
+                screen.blit(self.head, block_rect)
+            elif index == len(self.body) - 1:
+                screen.blit(self.tail, block_rect)
+            else:
+                prev_block = self.body[index + 1] - block
+                next_block = self.body[index - 1] - block
+
+                if prev_block.x == next_block.x:
+                    screen.blit(self.body_vertical, block_rect)
+                elif prev_block.y == next_block.y:
+                    screen.blit(self.body_horizontal, block_rect)
+                else:
+                    if (prev_block.x == -1 and next_block.y == -1) or (prev_block.y == -1 and next_block.x == -1):
+                        screen.blit(self.body_tl, block_rect)
+                    elif (prev_block.x == 1 and next_block.y == -1) or (prev_block.y == -1 and next_block.x == 1):
+                        screen.blit(self.body_tr, block_rect)
+                    elif (prev_block.x == -1 and next_block.y == 1) or (prev_block.y == 1 and next_block.x == -1):
+                        screen.blit(self.body_bl, block_rect)
+                    elif (prev_block.x == 1 and next_block.y == 1) or (prev_block.y == 1 and next_block.x == 1):
+                        screen.blit(self.body_br, block_rect)
+
+    def update_head_graphics(self):
+        head_relation = self.body[1] - self.body[0]
+        if head_relation == Vector2(1, 0):
+            self.head = self.head_left
+        elif head_relation == Vector2(-1, 0):
+            self.head = self.head_right
+        elif head_relation == Vector2(0, 1):
+            self.head = self.head_up
+        elif head_relation == Vector2(0, -1):
+            self.head = self.head_down
+
+    def update_tail_graphics(self):
+        tail_relation = self.body[-2] - self.body[-1]
+        if tail_relation == Vector2(1, 0):
+            self.tail = self.tail_left
+        elif tail_relation == Vector2(-1, 0):
+            self.tail = self.tail_right
+        elif tail_relation == Vector2(0, 1):
+            self.tail = self.tail_up
+        elif tail_relation == Vector2(0, -1):
+            self.tail = self.tail_down
 
     def move_snake(self):
-        if self.new_block == True:
+        if self.new_block:
             body_copy = self.body[:]
             body_copy.insert(0, body_copy[0] + self.direction)
             self.body = body_copy[:]
@@ -34,15 +114,13 @@ class SNAKE:
 
 class FRUIT:
     def __init__(self):
-        self.image = pygame.image.load(
-            'graphics/apple.png').convert_alpha()
-        self.image = pygame.transform.scale(
-            self.image, (cell_size, cell_size))
+        self.image = pygame.image.load('graphics/apple.png').convert_alpha()
+        self.image = pygame.transform.scale(self.image, (cell_size, cell_size))
         self.randomize()
 
     def draw_fruit(self):
         screen.blit(self.image, (int(self.pos.x * cell_size),
-                                 int(self.pos.y * cell_size)))
+                    int(self.pos.y * cell_size)))
 
     def randomize(self):
         self.x = random.randint(0, cell_number - 1)
@@ -57,6 +135,8 @@ class MAIN:
 
     def update(self):
         self.snake.move_snake()
+        self.snake.update_head_graphics()
+        self.snake.update_tail_graphics()
         self.check_collision()
         self.check_fail()
 
@@ -70,7 +150,10 @@ class MAIN:
             self.snake.add_block()
 
     def check_fail(self):
-        if not 0 <= self.snake.body[0].x < cell_number or not 0 <= self.snake.body[0].y < cell_number:
+        head = self.snake.body[0]
+        if not 0 <= head.x < cell_number or not 0 <= head.y < cell_number:
+            self.game_over()
+        if head in self.snake.body[1:]:
             self.game_over()
 
     def game_over(self):
@@ -78,6 +161,7 @@ class MAIN:
         sys.exit()
 
 
+# Game setup
 pygame.init()
 cell_size = 40
 cell_number = 20
@@ -89,6 +173,8 @@ SCREEN_UPDATE = pygame.USEREVENT
 pygame.time.set_timer(SCREEN_UPDATE, 150)
 
 main_game = MAIN()
+
+# Game loop
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
